e8456662924bacaf2290a2bc67691cd22a908c97
Ameni Alsaydi <amenialsaydi@pursuit.org>
2019-11-01 11:10:56 -0400

    first commit

--- TicTacToe/TicTacToe/GameButton.swift
+++ TicTacToe/TicTacToe/GameButton.swift
@@ -8,7 +8,15 @@
 
 import UIKit
 
-class GameButton: UIButton {
+// adding more attributes to a button
+// we want to be able to inspect the row and column of a button
+
+// we must change the button class from the default UIbutton to GameButton and then we can reference GameButton.row and GameButton.column to identify the  button selected by player. 
+
+class GameButton: UIButton { // inherits from UIButton
   @IBInspectable var row: Int = 0
   @IBInspectable var col: Int = 0
+    
+    //outlet a connection to a property
+    //action a connection to a method/function
 }
--- TicTacToe/TicTacToe/ViewController.swift
+++ TicTacToe/TicTacToe/ViewController.swift
@@ -9,12 +9,33 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+// all outlets should be above the override!!!!
+    // an outlet collection: an array of buttons
+    @IBOutlet var gameButtons: [GameButton]!
+    
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
   }
+    
+    //actions should be under the override!!!!
+    
+    
 
-
+    @IBAction func gameButtonPressed(_ gameButton: GameButton) {
+        print(" Row: \(gameButton.row) and Col: \(gameButton.col) were selected")
+    }
+    
 }
 
+// everytime the players select a button we have to update our board/ model
+// SF Symbol
+
+// X and O are background images
+
+// attributes inspectpor
+// size inspector
+
+// ishidden property (to check if the spot is still empty)
+
+// use a tic tac toe grid for the veiw 
