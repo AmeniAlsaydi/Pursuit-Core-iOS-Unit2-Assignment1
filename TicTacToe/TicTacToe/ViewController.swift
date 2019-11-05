969bfd8a28418ab80ffbf6c097b12a0d7b2f96d6
Ameni Alsaydi <amenialsaydi@pursuit.org>
2019-11-05 17:30:28 -0500

    functions have now been moved to tictactoebrain class

--- TicTacToe/TicTacToe/ViewController.swift
+++ TicTacToe/TicTacToe/ViewController.swift
@@ -9,82 +9,41 @@
 import UIKit
 
 class ViewController: UIViewController {
-// all outlets should be above the override!!!!
-    // an outlet collection: an array of buttons
+    
     @IBOutlet var gameButtons: [GameButton]!
     @IBOutlet weak var playerTurn: UILabel!
     var twoDArray = [["_","_","_"],["_","_","_"],["_","_","_"]]
+    
     var player = 1
     
+    var game = TicTacToeBrain() 
     
     override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
   }
     
-    //actions should be under the override!!!!
+    
     
     @IBAction func gameButtonPressed(_ gameButton: GameButton) {
 
-        let config = UIImage.SymbolConfiguration(pointSize: 25, weight: .light, scale: .large)
-        let xmarkImage = UIImage(systemName: "xmark", withConfiguration: config)
-        let circleImage = UIImage(systemName: "circle", withConfiguration: config)
         
-        //if twoDArray[gameButton.row][gameButton.col] == "_" {
-        //  allow the button to be pressed else dont.
-        
-        if player == 1 {
-            twoDArray[gameButton.row][gameButton.col] = "X"
-            
-            gameButton.setBackgroundImage(xmarkImage, for: .normal)
-            player = 2
-            playerTurn.text = "player two turn"
-        } else {
-            twoDArray[gameButton.row][gameButton.col] = "O"
-            gameButton.setBackgroundImage(circleImage, for: .normal)
-            player = 1
-            playerTurn.text = "player one turn"
-        }
-//
-//        } else {
-//            gameButton.isEnabled = false
-//        }
-        //print(" Row: \(gameButton.row) and Col: \(gameButton.col) were selected")
+        playerTurn.text = game.populate2DArray(gameButton)
         gameButton.isEnabled = false
-        print(twoDArray)
         
+        print(game.twoDArray)
+        // checkWinner()
+        playerTurn.text = game.checkWinner()
+        if game.checkWinner() != "" {
+            gameButtons.forEach{$0.isEnabled = false}
+        }
+    
     }
-   // func checkWinner
-    // if there are 3 in a row
-       
-   
+    
+    // new game: reset the twoDarray to "_"
+
 }
 
 
-// NOTES:
-// everytime the players select a button we have to update our board/ model
-// SF Symbol
-
-// X and O are background images
-
-// attributes inspectpor
-// size inspector
-
-// ishidden property (to check if the spot is still empty)
-
-// use a tic tac toe grid for the veiw 
-
-/*
-Multi (2) dimensional array to keep track  of the board
-
-0,0  0,1  0,2
-1,0   1,1  1,2
-2,0  2,1  2,2
 
 
-Identity inspector tells us about the class of the button
-We change the class of our buttons to GameButton, now our buttons have row and col
-
-
-//GameButton inherits from button which inherits from control which inherits from view.
-*/ 
