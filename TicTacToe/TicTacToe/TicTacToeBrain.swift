6a066e95a5b4858fbf6bd315b9a5439694977163
Ameni Alsaydi <amenialsaydi@pursuit.org>
2019-11-06 14:31:50 -0500

    game fully functional

--- TicTacToe/TicTacToe/TicTacToeBrain.swift
+++ TicTacToe/TicTacToe/TicTacToeBrain.swift
@@ -10,78 +10,115 @@ import Foundation
 import UIKit
 
 class TicTacToeBrain {
-    var twoDArray = [["_","_","_"],["_","_","_"],["_","_","_"]]
+    var buttonMattrix = [["_","_","_"],["_","_","_"],["_","_","_"]]
     var player = 1
+    // var playerTurn = ""
     
-    func populate2DArray(_ gameButton: GameButton) -> String {
+    func populateMattrix(_ gameButton: GameButton) -> String {
         
         let config = UIImage.SymbolConfiguration(pointSize: 25, weight: .light, scale: .large)
         let xmarkImage = UIImage(systemName: "xmark", withConfiguration: config)
         let circleImage = UIImage(systemName: "circle", withConfiguration: config)
+        var returnLabel: String = ""
         
-        var playerTurn = ""
-        
-        if player == 1 {
-            twoDArray[gameButton.row][gameButton.col] = "X"
+        // if the matrix is full and has no "_"
             
-            gameButton.setBackgroundImage(xmarkImage, for: .normal)
-            player = 2
-            playerTurn = "player two turn"
-        } else {
-            twoDArray[gameButton.row][gameButton.col] = "O"
-            gameButton.setBackgroundImage(circleImage, for: .normal)
-            player = 1
-            playerTurn = "player one turn"
-        }
-        return playerTurn 
+            if player == 1 {
+                buttonMattrix[gameButton.row][gameButton.col] = "X"
+                gameButton.setBackgroundImage(xmarkImage, for: .normal)
+                player = 2
+                returnLabel = "player two turn"
+            } else {
+                buttonMattrix[gameButton.row][gameButton.col] = "O"
+                gameButton.setBackgroundImage(circleImage, for: .normal)
+                player = 1
+                returnLabel = "player one turn"
+            }
+            
+    
+        return returnLabel
     }
-
+    
     
     func checkWinner()  -> String {
         var winner: String = ""
         
         for col in 0...2 {
-               // checking columns
-                      // col = 0
-                      // [0][0] [1][0] [2][0]
-               
-               if twoDArray[0][col] == "X" &&
-                   twoDArray[1][col] == "X" &&
-                   twoDArray[2][col] == "X" {
-                   print(" X wins ")
-                   winner = "PLAYER ONE WINS 🥳"
-
-               }
-               if twoDArray[0][col] == "O" &&
-                   twoDArray[1][col] == "O" &&
-                   twoDArray[2][col] == "O" {
-                   print(" O wins ")
-                   winner = "PLAYER TWO WINS 🥳"
-
-               }
-         
-         }
-           for row in 0...2 {
-                 // checking rows
-                        // row = 0
-                        // [0][0] [0][1] [0][2]
-                 
-                 if twoDArray[row][0] == "X" &&
-                     twoDArray[row][1] == "X" &&
-                     twoDArray[row][2] == "X" {
-                     print(" X wins ")
-                   winner = "PLAYER ONE WINS 🥳"
-
-                 }
-                 if twoDArray[row][0] == "O" &&
-                     twoDArray[row][1] == "O" &&
-                     twoDArray[row][2] == "O" {
-                     print(" O wins ")
-                   winner = "PLAYER TWO WINS 🥳"
-                 }
-           
-           }
-           
-           return winner
+            // checking columns
+            // col = 0
+            // [0][0] [1][0] [2][0]
+            
+            if buttonMattrix[0][col] == "X" &&
+                buttonMattrix[1][col] == "X" &&
+                buttonMattrix[2][col] == "X" {
+                print(" X wins ")
+                winner = "PLAYER ONE WINS 🥳"
+                
+            }
+            if buttonMattrix[0][col] == "O" &&
+                buttonMattrix[1][col] == "O" &&
+                buttonMattrix[2][col] == "O" {
+                print(" O wins ")
+                winner = "PLAYER TWO WINS 🥳"
+                
+            }
+            
         }
+        for row in 0...2 {
+            // checking rows
+            // row = 0
+            // [0][0] [0][1] [0][2]
+            
+            if buttonMattrix[row][0] == "X" &&
+                buttonMattrix[row][1] == "X" &&
+                buttonMattrix[row][2] == "X" {
+                print(" X wins ")
+                winner = "PLAYER ONE WINS 🥳"
+                
+            }
+            if buttonMattrix[row][0] == "O" &&
+                buttonMattrix[row][1] == "O" &&
+                buttonMattrix[row][2] == "O" {
+                print(" O wins ")
+                winner = "PLAYER TWO WINS 🥳"
+            }
+            
+        }
+        
+        if buttonMattrix[0][0] == "X" &&
+            buttonMattrix[1][1] == "X" &&
+            buttonMattrix[2][2] == "X" {
+            print(" X wins ")
+            winner = "PLAYER ONE WINS 🥳"
+            
+        } else if buttonMattrix[0][0] == "O" &&
+            buttonMattrix[1][1] == "O" &&
+            buttonMattrix[2][2] == "O" {
+            print(" O wins ")
+            winner = "PLAYER TWO WINS 🥳"
+            
+        }
+
+            
+        if buttonMattrix[2][0] == "O" &&
+            buttonMattrix[1][1] == "O" &&
+            buttonMattrix[0][2] == "O" {
+            print(" O wins ")
+            winner = "PLAYER TWO WINS 🥳"
+            
+        } else if buttonMattrix[2][0] == "X" &&
+            buttonMattrix[1][1] == "X" &&
+            buttonMattrix[0][2] == "X" {
+            print(" O wins ")
+            winner = "PLAYER ONE WINS 🥳"
+        }
+        
+        return winner
+    }
+    func reset() {
+        buttonMattrix = [["_","_","_"],["_","_","_"],["_","_","_"]]
+        player = 1
+    }
 }
+
+
--- TicTacToe/TicTacToe/ViewController.swift
+++ TicTacToe/TicTacToe/ViewController.swift
@@ -13,8 +13,9 @@ class ViewController: UIViewController {
     @IBOutlet var gameButtons: [GameButton]!
     @IBOutlet weak var playerTurn: UILabel!
     var twoDArray = [["_","_","_"],["_","_","_"],["_","_","_"]]
+    var turn: String = ""
+    var winning: String = ""
     
-    var player = 1
     
     var game = TicTacToeBrain() 
     
@@ -23,27 +24,49 @@ class ViewController: UIViewController {
     // Do any additional setup after loading the view, typically from a nib.
   }
     
-    
-    
     @IBAction func gameButtonPressed(_ gameButton: GameButton) {
 
+        turn = game.populateMattrix(gameButton)
+        winning = game.checkWinner()
         
-        playerTurn.text = game.populate2DArray(gameButton)
+        if game.buttonMattrix.joined().contains("_") {
+            if winning == "" { // no one has won
+                         playerTurn.text = turn
+                   } else {
+                     gameButtons.forEach{$0.isEnabled = false}
+                        playerTurn.text = winning
+                   }
+            
+        } else {
+            playerTurn.text = "NO WINNER"
+        }
         gameButton.isEnabled = false
-        
-        print(game.twoDArray)
-        // checkWinner()
-        playerTurn.text = game.checkWinner()
-        if game.checkWinner() != "" {
-            gameButtons.forEach{$0.isEnabled = false}
+        print(game.buttonMattrix)
         }
     
+    // new game: reset the twoDarray to "_"
+    
+    @IBAction func playAgain(_ sender: UIButton) {
+
+        game.reset()
+        turn = ""
+        winning = ""
+        // need to check the top label back to "tic tac toe"
+        playerTurn.text = "TIC TAC TOE"
+        // need to clear board
+        for button in gameButtons {
+            button.setBackgroundImage( nil, for: .normal)
+            button.isEnabled = true
+        }
+        print(game.buttonMattrix)
+// i cant play again
     }
     
-    // new game: reset the twoDarray to "_"
 
 }
 
+//TODO:
+// curve the image edges.
 
 
 
